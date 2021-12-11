3dca3fc410980858cf2abb65263f61bd2d90e5e4
Jottero <lookslikecat@gmail.com>
2021-12-11 17:11:26 +0300

    initial commit

---
+++ src/christmas.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Christmas extends Component {
+    render() {
+        return (
+            <pre>
+             *
+            /.\
+           /..'\
+            /'.'\
+          /.''.'\
+          /.'.'.\
+   "'""""/'.''.'.\""'"'"
+     jgs ^^^[_]^^^
+            </pre>
+        )
+    }
+}
+
